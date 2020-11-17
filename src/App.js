80ff4cb1347b8bd2593c633bba1f084236f4747a
Terhi <salonenterhi19@gmail.com>
2020-11-17 09:06:06 +0200

    Import react app into MedoDodo-front.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <p>Hello world</p>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
